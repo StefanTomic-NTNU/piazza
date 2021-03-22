d97126bba1775e71bdb97d1b7656f7dcec9b9939
anon <anon@email.com>
2021-03-23 00:41:47 +0100

    added TODO for next methods

--- src/main/java/tdt4145/core/ThreadDAO.java
+++ src/main/java/tdt4145/core/ThreadDAO.java
@@ -8,6 +8,13 @@ import java.util.Arrays;
 
 public class ThreadDAO extends TemplateDAO {
     private Connection connection;
+    //TODO create get all TAGS in one method
+    //TODO create get all Courses int table Course
+    //TODO maybe create a method for instructor priviligies
+    //TODO create search function
+    //TODO get statistics on user if user has priviligies, created post, watched posts
+    //The output is “user name, number of posts read, number of posts created
+    // The result should also include users which have not read or created posts.
 
     public ThreadDAO() throws SQLException {
         this.connection = super.getConnection();
