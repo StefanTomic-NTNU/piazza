1468c55878890326506930cd9fd083e2ff4474b2
anon <anon@email.com>
2021-03-24 23:19:07 +0100

    fix

--- src/main/java/tdt4145/ui/Main.java
+++ src/main/java/tdt4145/ui/Main.java
@@ -73,7 +73,7 @@ public class Main {
             System.out.println("Select option");
             System.out.println("-------------------------");
             System.out.println("1 - List all posts");
-            System.out.println("2 - Search for threads");
+            System.out.println("2 - Search for posts");
             System.out.println("3 - Open post");
             System.out.println("4 - Create new thread");
             System.out.println("5 - Quit");
